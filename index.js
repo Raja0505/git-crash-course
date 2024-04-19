a9c17030c422d8831bf71113ff52452d293d3ca1
Mohammed Raja <142135060+Raja0505@users.noreply.github.com>
2024-04-19 16:03:40 +0530

    Buggy code added

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const name = "mohammed raja"
-const age = "26/01/2002"
+const name = "mohammed raja";
+const age = "26/01/2002";
+
+const newfn = () => {
+  console.log("This is the new fun");
+};
+
+newfn();
--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const name = "husain zari wala";
-const age = "26/july/2002";
+const age1 = "26/july/2002";
+
+function buggy(a, b) {
+  return a + b;
+}
+console.log(buggy(5, 7));
